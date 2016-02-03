be4a5d9bf5fff9faba8d6844c47e3cb06059e8d4
Miztaoak <ek.johan98@gmail.com>
2016-02-03 12:54:59 +0100

    Ladde till en ny databas, Länkade den med hjälp av php och rensade i index.php

---
+++ mysql_connect_.php
@@ -0,0 +1,21 @@
+<?php
+
+	DEFINE('DB_USER','root');
+	DEFINE('DB_HOST','localhost');
+	DEFINE('DB_PASSWORD','j0h4ng04t');
+	DEFINE('DB_DATABASE','web_goaty');
+		
+		
+	$dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+?>
