e91e9872c4345c25cafa009ca2d3b17385e84657
Miztaoak <ek.johan98@gmail.com>
2016-02-03 13:36:47 +0100

    Uppgift php 1

--- www/index.php
+++ www/index.php
@@ -1,3 +1,49 @@
 <?php
+	require_once('../mysql_connect_.php');
 
+	$query = 'SELECT * FROM Cities WHERE Population>5000000 ORDER BY Population DESC';
+
+	$result = mysqli_query($dbc, $query);
+
+	echo 'Uppgift 1<br>';
+
+	echo '<table border = "solid">';
+
+		while($rad = mysqli_fetch_array($result)){
+			echo '<tr><td>' . ucfirst($rad['City']) . '</td> <td>' . $rad['Population'] . '</td> </tr>';
+		}
+
+	echo '</table>';
+
+	echo '<br>';
+
+	echo 'Uppgift 2<br>';
+
+	$query = 'SELECT * FROM Cities WHERE Longitude>=0 ORDER BY City ASC';
+
+	$result = mysqli_query($dbc, $query);
+
+	echo '<table border = "solid">';
+
+		while($rad = mysqli_fetch_array($result)){
+			echo '<tr><td>' . ucfirst($rad['City']) . '</td> <td>' . $rad['Population'] . '</td> </tr>';
+		}
+
+	echo '</table>';
+
+	echo '<br>';
+
+	echo 'Uppgift 3<br>';
+
+	$query = 'SELECT * FROM Cities WHERE Country="br" ORDER BY City ASC';
+
+	$result = mysqli_query($dbc, $query);
+
+	echo '<table border = "solid">';
+
+		while($rad = mysqli_fetch_array($result)){
+			echo '<tr><td>' . ucfirst($rad['City']) . '</td> <td>' . $rad['Population'] . '</td> </tr>';
+		}
+
+	echo '</table>';
 ?>
